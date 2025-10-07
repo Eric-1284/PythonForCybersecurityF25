9529ae59ecd539590722c33c1f7cc41421bb5bd0
Eric-1284 <wami.eric@student.greenriver.edu>
2025-10-07 13:22:43 -0700

    Edit age

--- end/CH02/HelloWorld2.py
+++ end/CH02/HelloWorld2.py
@@ -4,3 +4,6 @@
 
 your_name = input("What is your name? ")
 print("Hello {0}".format(your_name))
+your_age = int(input("What is your age "))
+future_age = your_age + 2
+print("your age is gonna be" + str(future_age))
